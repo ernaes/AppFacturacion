d24a4d69d637527f8ec4b63a2f4faa07b663493f
Ernaes Trujillo <Ernaes Trujillo@DESKTOP-IBIFLMN>
2021-05-31 19:29:41 -0500

    Agregar archivos de proyecto.

---
+++ AppFacturacion/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFacturacion
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string rfc { get; set; }
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+
+        public string nombreCompleto()
+        {
+            return nombre + " " + apellidoPaterno + " " + apellidoMaterno;
+        }
+    }
+}
---
+++ AppFacturacion/ConexionDb.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+
+namespace AppFacturacion
+{
+    public class ConexionDb
+    {
+        public static MySqlConnection ObtenerConexion()
+        {
+            string cs = @"server=localhost;userid=root;password=123456;database=db_facturacion";
+
+            MySqlConnection con = new MySqlConnection(cs);
+            con.Open();
+            return con;
+        
+        }
+
+        
+    }
+}
